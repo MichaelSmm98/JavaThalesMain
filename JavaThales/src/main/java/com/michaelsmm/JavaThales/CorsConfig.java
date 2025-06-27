1ab5f4866277b43f54af2d80d909de8e78693567
Michael Mendieta <50148082+MichaelSmm98@users.noreply.github.com>
2025-06-26 23:54:52 -0500

    Second Commit

---
+++ JavaThales/src/main/java/com/michaelsmm/JavaThales/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.michaelsmm.JavaThales;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+    @Configuration
+    public class CorsConfig implements WebFluxConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:4200")
+                    .allowedMethods("");
+        }
+    }
+
